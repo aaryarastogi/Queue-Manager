67d1d264e2fa59cb38995544b70af2110688bcb0
Aarya Rastogi <97822457+aaryarastogi@users.noreply.github.com>
2025-09-29 19:54:19 +0530

    get / route added

--- server/index.js
+++ server/index.js
@@ -20,6 +20,10 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get('/', (req,res)=>{
+  return res.send("server is live")
+})
+
 app.post('/register', registerHandler);
 app.post("/login", loginHandler);
 
